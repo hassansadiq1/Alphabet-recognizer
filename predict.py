98164fa377a7b62c3fc8c8c38eb7665936707eb4
hassansadiq1 <44541936+hassansadiq1@users.noreply.github.com>
2018-11-19 00:18:28 +0500

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
+# It takes grayscale images and predict alphabet
 import numpy as np
 from keras.models import load_model
 from scipy.misc import imresize
-model = load_model('my_model.h5')
+model = load_model('.h5')
 
 def preprocess_image(img):  
   img = imresize(img,(28,28))
@@ -9,7 +10,8 @@ def preprocess_image(img):
   result = np.expand_dims(img,axis=3)
   return result
 
-image = None#load image
+image = None#load your image
 pre_image = preprocess_image(image)
 y = model.predict(pre_image)
+y = np.argmax(y,axis = 1)
 print(y)
