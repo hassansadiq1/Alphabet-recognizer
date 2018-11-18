da44b27d1c4de2705b9c57983bd43fb2e4fb24a4
hassansadiq1 <44541936+hassansadiq1@users.noreply.github.com>
2018-11-19 00:09:44 +0500

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,15 @@
+import numpy as np
+from keras.models import load_model
+from scipy.misc import imresize
+model = load_model('my_model.h5')
+
+def preprocess_image(img):  
+  img = imresize(img,(28,28))
+  img = np.expand_dims(img,axis=0)
+  result = np.expand_dims(img,axis=3)
+  return result
+
+image = None#load image
+pre_image = preprocess_image(image)
+y = model.predict(pre_image)
+print(y)
