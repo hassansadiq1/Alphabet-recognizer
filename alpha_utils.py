4f41bdc3ebbeb00ebf5248cdef28ae5562d05501
hassansadiq1 <44541936+hassansadiq1@users.noreply.github.com>
2018-11-19 12:03:38 +0500

    Update alpha_utils.py

--- alpha_utils.py
+++ alpha_utils.py
@@ -12,7 +12,8 @@ def decoder(prediction):
   x = np.argmax(prediction, axis = 1)
   label_encoder = preprocessing.LabelEncoder()
   label_encoder.fit(['A','B','C','D','E','F','G','H','I','J','K','L','M',
-        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+                     'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                     '0','1','2','3','4','5','6','7','8','9'])
   result = label_encoder.inverse_transform([x])
   if len(prediction) == 1:
     result = result[0]
