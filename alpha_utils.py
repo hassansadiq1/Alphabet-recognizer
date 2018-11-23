fd8da4c4994ea43f10ff2da71b7396c74a730665
hassansadiq1 <44541936+hassansadiq1@users.noreply.github.com>
2018-11-23 23:34:23 +0500

    Update alpha_utils.py

--- alpha_utils.py
+++ alpha_utils.py
@@ -2,7 +2,8 @@ import numpy as np
 import cv2 as cv
 from sklearn import preprocessing
 
-def preprocess_image(img):  
+def preprocess_image(img):
+  img = np.array(img, dtype=np.uint8)
   img = cv.resize(img,None,(28,28), interpolation = cv.INTER_CUBIC)
   img = np.expand_dims(img,axis=0)
   result = np.expand_dims(img,axis=3)
